388029f1b1295b82148d9ad03c66a89ac0804c7e
hudziol <hudziolxd@gmail.com>
2020-05-13 19:27:20 +0200

    0.0.1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,50 @@
+function setCookie(cname, cvalue, exdays)
+{
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+  
+function getCookie(cname)
+{
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++)
+    {
+        var c = ca[i];
+        while (c.charAt(0) == ' ')
+        {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0)
+        {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie()
+{
+    var user = getCookie("username");
+    if (user != "")
+    {
+        alert("Welcome again " + user);
+    }
+    else
+    {
+        user = prompt("Please enter your name:", "");
+        if (user != "" && user != null)
+        {
+            setCookie("username", user, 365);
+        }
+    }
+}
+
 var slid = 1;
 var max = 3;
-setTimeout(slider, 2000);
+setTimeout(slider, 1);
 function slider()
 {
     if(slid < max)
@@ -8,8 +52,10 @@ function slider()
         for(i = 1; i <= max; i++)
         {
             document.getElementById("slid-" + i).style.display = "none";
+            document.getElementById("slid-title-" + i).style.display = "none";
         }
         document.getElementById("slid-" + slid).style.display = "block";
+        document.getElementById("slid-title-" + slid).style.display = "block";
         slid ++;
     }
     else if(slid == max)
@@ -17,8 +63,10 @@ function slider()
         for(i = 1; i <= max; i++)
         {
             document.getElementById("slid-" + i).style.display = "none";
+            document.getElementById("slid-title-" + i).style.display = "none";
         }
         document.getElementById("slid-" + slid).style.display = "block";
+        document.getElementById("slid-title-" + slid).style.display = "block";
         slid = 1;
     }
     setTimeout(slider, 2000);
