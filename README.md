04f844f2d1d14c5e798486645f2e98ca8aab8e1d
hudziol <hudziolxd@gmail.com>
2020-05-25 15:37:54 +0200

    0

--- README.md
+++ README.md
@@ -2,6 +2,6 @@ Oficjalna strona internetowa dotycząca spraw Hudziola.
 
 W celu kontaktu z autorem zapraszam na DM Discord'a Hudziol#1581
 
-Wszelkie treści zawarte na każdej podstronie są objęte prawami autorskimi.
+Wszelkie treści zawarte na każdej podstronie są objęte prawami autorskimi natomiast zezwalam na używanie kodu w celach własnych.
 
-Copyright Dawid Hudzia 2020
+Copyright Dawid Hudzia | 2020
--- js/script.js
+++ js/script.js
@@ -72,6 +72,24 @@ function slider()
     setTimeout(slider, 4000);
 }
 
+//var img = this.document.getElementById("img-1");
+//var img = document.getElementsByClassName("galer-img");
+//var img = document.querySelectorAll(".galer-img");
+//var num;
+//img.onclick = imgMax;
+/* for(i=0; i<10; i++)
+{
+    img[i].onclick = imgMax;
+    console.log(i);
+}
+
+function imgMax()
+{
+    console.log("Img");
+    //var imgScreen = document.createElement("div");
+    //imgScreen.src;
+} */
+
 window.onload = alert;
 
 function alert(klik)
@@ -90,7 +108,15 @@ function alert(klik)
         }
         else
         {
-            document.getElementById("alert").style.display = "block";
+            //document.getElementById("alert").style.display = "block";
         }
     }
+}
+
+function wizyty()
+{
+    var liczbaWejsc = Number(getCookie("visit"));
+    var wejscia = liczbaWejsc + 1;
+    setCookie("visit", wejscia, "9999")
+    console.log(wejscia);
 }
